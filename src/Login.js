abb9d7d91ba29babb526920ea43436caa67ebacb
gathoni-k <maryg.kahiga@gmail.com>
2020-08-17 21:16:42 +0300

    feat: Redirect authenticated users to the dashboard page
    From the UserContext, get the user and check their value
    if the user is truthy redirect them

--- src/Login.js
+++ src/Login.js
@@ -1,7 +1,20 @@
 import React, { useEffect, useContext, useState } from 'react';
 import './Login.css'
 import { signInWithGoogle } from './services/firebase';
+import { UserContext } from './providers/UserProvider';
+import { Redirect } from 'react-router-dom';
 export default function Login() {
+  const user = useContext(UserContext)
+  const [redirect, setredirect] = useState(null)
+
+  useEffect(() => {
+    if (user) {
+      setredirect('/dashboard')
+    }
+  }, [user])
+  if (redirect) {
+    return <Redirect to={redirect}/>
+  }
   return (
       <div className="login-buttons">
         <button className="login-provider-button" onClick={signInWithGoogle}>
