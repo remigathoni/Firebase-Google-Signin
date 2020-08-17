b0900d4af4a50039b3f8157317c8394a672190da
gathoni-k <maryg.kahiga@gmail.com>
2020-08-17 21:20:12 +0300

    feat: Redirect unauthenticated user to the login page
    Use UserContext to get user value. If the user is unauthenticated, set redirect state to the home path

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import './Dashboard.css'
+import React, { useEffect, useContext, useState } from "react";
+import "./Dashboard.css";
+import { UserContext } from "./providers/UserProvider";
+import { Redirect } from "react-router-dom";
 export default function Dashboard() {
-  return (
-      <div className="dashboard">
-      	<h1 className="dashboard-text">Welcome Home</h1>
-        <button className="logout-button">
+  const user = useContext(UserContext);
+  const [redirect, setredirect] = useState(null);
+  useEffect(() => {
+    if (!user) {
+      setredirect("/");
+    }
+  }, [user]);
+  if (redirect) {
+    return <Redirect to={redirect} />;
+  }
 
-        <img src="https://img.icons8.com/ios-filled/50/000000/google-logo.png" alt="google icon"/>
+  return (
+    <div className="dashboard">
+      <h1 className="dashboard-text">Welcome Home</h1>
+      <button className="logout-button">
+        <img
+          src="https://img.icons8.com/ios-filled/50/000000/google-logo.png"
+          alt="google icon"
+        />
         <span> logout</span>
-       </button>
-      </div>
+      </button>
+    </div>
   );
 }
