93c9f83856ac19dcaec935b6b43b88151e09daf6
gathoni-k <maryg.kahiga@gmail.com>
2020-08-17 21:14:16 +0300

    feat: Signin user through google
    Import the signInWithGoogle function from firebase and use it in the onClick event in the sign in button

--- src/Login.js
+++ src/Login.js
@@ -1,17 +1,13 @@
-import React from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import './Login.css'
+import { signInWithGoogle } from './services/firebase';
 export default function Login() {
   return (
       <div className="login-buttons">
-        <button className="login-provider-button">
-
+        <button className="login-provider-button" onClick={signInWithGoogle}>
         <img src="https://img.icons8.com/ios-filled/50/000000/google-logo.png" alt="google icon"/>
         <span> Continue with Google</span>
        </button>
-        <button className="login-provider-button">
-        <img src="https://img.icons8.com/ios-filled/50/000000/facebook-circled.png" alt="facebook icon"/>
-        <span>Continue with FaceBook</span>
-        </button>
       </div>
   );
 }
